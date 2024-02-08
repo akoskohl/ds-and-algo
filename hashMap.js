55e0cac33e586cd60fdacc6ea69c733beb752160
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-02-08 14:16:10 +0100

    helper functions to fill table

--- hashMap.js
+++ hashMap.js
@@ -10,3 +10,22 @@ class HashTable {
         return hash; 
     }
 }
+// helper functions
+
+function fillHashTable(int) {
+    for (let i = 0; i < int; i++) {
+        rand1 = generateRandomString(10);
+        rand2 = generateRandomString(10);
+        myHashTable.set(rand1, rand2);    
+    }
+}
+
+function generateRandomString(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
