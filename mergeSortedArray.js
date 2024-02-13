b29627408bd476e8c68185ec23fa1e79011d407d
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-02-13 09:55:37 +0100

    helper random generator

--- mergeSortedArray.js
+++ mergeSortedArray.js
@@ -1,3 +1,8 @@
 // Description: Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
 // sortedArray = mergeSortedArray(array1, array2)
 
+function fillArrayWithRandomIntigers(array, amount) {
+    for (let i = 0; i < amount; i++) {
+        array.push(Math.floor(Math.random() * 100));
+    }
+}
