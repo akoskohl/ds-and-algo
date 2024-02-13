f03e74a53b743fc76071649b712ac0676f2bd2bb
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-02-13 08:55:52 +0100

    Create firstRecurringCharacter.js

---
+++ firstRecurringCharacter.js
@@ -0,0 +1,12 @@
+//Google Question
+// Return first recurring character
+
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+// return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+// return 1
+
+//Given an array = [2,3,4,5]:
+// return undefined
+
