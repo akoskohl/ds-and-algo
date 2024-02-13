e23ead46eba2c65308346e45a2ca0e2aceb93ac7
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-02-13 08:56:46 +0100

    function

--- firstRecurringCharacter.js
+++ firstRecurringCharacter.js
@@ -15,3 +15,15 @@ array2 = [2,1,1,2,3,5,1,2,4]
 array3 = [2,3,4,5]
 array4 = []
 
+function firstRecurringCharacter(input) {
+    let map = [];
+    for (let i = 0; i < input.length; i++) {
+        if (map.includes(input[i])) {
+            return input[i]
+        }
+        else {
+            map.push(input[i]);
+        }
+    }
+    console.log("This array has no recurring char.")
+}
