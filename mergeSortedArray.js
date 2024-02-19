2f018d12f87177151e2936b4dfdcbffee287d1f0
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-02-19 09:30:15 +0100

    Update mergeSortedArray.js

--- mergeSortedArray.js
+++ mergeSortedArray.js
@@ -1,6 +1,17 @@
 // Description: Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
 // sortedArray = mergeSortedArray(array1, array2)
 
+// helper functions
+
+// Testing my solution
+const concatedAndSortedArray = array1.concat(array2).sort(function(a,b){return a - b})
+
+// check if the sortedArray is equal to concatedAndSortedArray as a test
+if (JSON.stringify(concatedAndSortedArray) == JSON.stringify(sortedArray))
+    console.log("TEST: OK");
+else
+    console.log("TEST: NOT OK");
+
 function fillArrayWithRandomIntigers(array, amount) {
     for (let i = 0; i < amount; i++) {
         array.push(Math.floor(Math.random() * 100));
