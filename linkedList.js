3f78d151e039b5968e517ad500cc484aeedbff0f
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-02-23 17:03:08 +0100

    prettier messed it up

--- linkedList.js
+++ linkedList.js
@@ -1,32 +1,61 @@
 // create a linked list with a head and a tail, starting reference is 10->5->16
 
 class Node {
-    constructor(value) {
-        this.value = value
-        this.next = null
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class LinkedList {
-    constructor(value) {
-        this.head = {
-            value: value,
-            next: null
-        }
-        this.tail = this.head;
-        this.length = 1;
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+  }
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+  }
+  insert(index, value) {
+    if (index >= this.length) {
+      console.log("index is out of bound, appended to the end");
+      return this.append(value);
     }
-    append(value) {
-        const newNode = new Node(value)
-        this.tail.next = newNode;
-        this.tail = newNode;
-        this.length++;
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const pointer = leader.next;
+    leader.next = newNode;
+    newNode.next = pointer;
+    this.length++;
+  }
+  remove(index) {
+    if (index >= this.length) {
+      console.log("index is out of bound, cannote remove anything");
     }
-    prepend(value){
-        const newNode = new Node(value)
-        newNode.next = this.head
-        this.head = newNode
-        this.length++
+    const leader = this.traverseToIndex(index - 1);
+    const pointer = this.traverseToIndex(index + 1);
+    leader.next = pointer;
+    this.length--;
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      // that will throw an error, tail should be handled differently
+      counter++;
     }
     insert (index, value) {
         if (index >= this.length) {
