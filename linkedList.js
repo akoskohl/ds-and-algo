de84561f6e6a46202ec905bec530b166a46f0f5f
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-02-23 17:03:38 +0100

    Update linkedList.js

--- linkedList.js
+++ linkedList.js
@@ -64,9 +64,16 @@ class LinkedList {
 }
 
 const myLinkedList = new LinkedList(10);
-myLinkedList.append(5)
-myLinkedList.append(16)
-myLinkedList.append(0)
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.append(0);
+
+console.log(myLinkedList.traverseToIndex(3));
+
+myLinkedList.prepend(1);
+
+myLinkedList.insert(2, 3);
+myLinkedList.insert(20, 28);
 
 console.log(myLinkedList.traverseToIndex(3))
 
